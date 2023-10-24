d1932ceb2f56fdd3627b7975c20aec36ed6d86d4
Wan  Hashim <whashim@gmail.com>
2023-10-24 17:28:23 +0800

    init

---
+++ app/Models/Customer.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Customer extends Model
+{
+    use HasFactory;
+    
+    protected $fillable = [
+        'name',
+        'email',
+        'address',
+    ];
+    
+    protected $hidden = [
+        'created_at',
+        'updated_at',
+    ];
+    
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
+}
